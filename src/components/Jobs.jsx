919d5dfddcebaab5cc7f6f571888edfa0fee5267
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-03-18 13:45:22 +0530

    Updated

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -11,20 +11,20 @@ import { useEffect, useState } from "react";
 // const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 const Jobs = () => {
     const { allJobs, searchedQuery } = useSelector(store => store.job);
-    const [filterJobs, setFilterJobs] = useState(allJobs);
+    // const [filterJobs, setFilterJobs] = useState(allJobs);
 
-    useEffect(() => {
-        if (searchedQuery) {
-            const filteredJobs = allJobs.filter((job) => {
-                return job.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
-                    job.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
-                    job.location.toLowerCase().includes(searchedQuery.toLowerCase())
-            })
-            setFilterJobs(filteredJobs)
-        } else {
-            setFilterJobs(allJobs)
-        }
-    }, [allJobs, searchedQuery]);
+    // useEffect(() => {
+    //     if (searchedQuery) {
+    //         const filteredJobs = allJobs.filter((job) => {
+    //             return job.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+    //                 job.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+    //                 job.location.toLowerCase().includes(searchedQuery.toLowerCase())
+    //         })
+    //         setFilterJobs(filteredJobs)
+    //     } else {
+    //         setFilterJobs(allJobs)
+    //     }
+    // }, [allJobs, searchedQuery]);
 
     return (
         <div>
@@ -35,7 +35,7 @@ const Jobs = () => {
                         <FilterCard />
                     </div>
                     {
-                        filterJobs.length <= 0 ? <span>Job not found</span> : (
+                        allJobs.length <= 0 ? <span>Job not found</span> : (
                             <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
                                 <div className='grid grid-cols-3 gap-4'>
                                     {
--- src/hooks/useGetAllJobs.jsx
+++ src/hooks/useGetAllJobs.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { setAllJobs } from '@/redux/jobSlice';
+import axios from 'axios'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 
 const useGetAllJobs = () => {
-  return (
-    <div>
-      useGetAllJobs
-    </div>
-  )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchAllJobs = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8000/api/job/get`, { withCredentials: true });
+        if (res.data.success) {
+          dispatch(setAllJobs(res.data.jobs));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchAllJobs();
+  }, [])
 }
 
 export default useGetAllJobs
