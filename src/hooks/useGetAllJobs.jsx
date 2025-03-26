4fcf85af29842ebeb85e76733fae63cef71f56cc
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-03-26 14:26:12 +0530

    files updated

--- src/hooks/useGetAllJobs.jsx
+++ src/hooks/useGetAllJobs.jsx
@@ -1,25 +1,34 @@
 import { setAllJobs } from '@/redux/jobSlice';
-import axios from 'axios'
-import { useEffect } from 'react';
+import axios from 'axios';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
-  const { searchedQuery } = useSelector(store => store.job);
-  useEffect(() => {
-    const fetchAllJobs = async () => {
-      try {
-        const res = await axios.get(`https://portal-server-cpms123.vercel.app/api/job/get?keyword=${searchedQuery}`, { withCredentials: true });
-        if (res.data.success) {
-          dispatch(setAllJobs(res.data.jobs));
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchAllJobs();
-  }, [])
-}
+  const { searchedQuery } = useSelector((store) => store.job);
 
-export default useGetAllJobs
+  const fetchAllJobs = useCallback(async () => {
+    try {
+      const res = await axios.get(
+        `https://portal-server-cpms123.vercel.app/api/job/get?keyword=${searchedQuery}`,
+        { withCredentials: true }
+      );
+
+      if (res.data.success) {
+        dispatch(setAllJobs(res.data.jobs));
+      }
+    } catch (error) {
+      console.error("Error fetching jobs:", error);
+    }
+  }, [searchedQuery, dispatch]);
+
+  useEffect(() => {
+    fetchAllJobs();
+
+    return () => {
+      
+    };
+  }, [fetchAllJobs]);
+};
+
+export default useGetAllJobs;
